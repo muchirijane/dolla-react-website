4cc32d6e92f4d78543c1323779a0bd27e779ca11
Jane Tracy Muthoni <muchirijane@outlook.com>
2020-11-30 18:59:59 +0300

    Updates

--- static/js/main.c3dd44c8.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpdolla-react-website"]=this["webpackJsonpdolla-react-website"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(0),s=c(1),r=c.n(s),p=c(3),a=c.n(p);c(9);var i=function(){return Object(n.jsx)("div",{className:"App",children:Object(n.jsx)("h1",{children:"What's uppppppp"})})};a.a.render(Object(n.jsx)(r.a.StrictMode,{children:Object(n.jsx)(i,{})}),document.getElementById("root"))},9:function(e,t,c){}},[[10,1,2]]]);
-//# sourceMappingURL=main.c3dd44c8.chunk.js.map
