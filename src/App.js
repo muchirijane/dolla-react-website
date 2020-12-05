59465f1321364efd061f97b5029e732e54125fd2
Jane Tracy Muthoni <muchirijane@outlook.com>
2020-12-05 14:37:01 +0300

    changed the index router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import {HashRouter as Router,Switch,Route} from "react-router-dom";
 import { Footer} from "./components";
 import GlobalStyle from "./GlobalStyles";
 import Home from "./pages/Home";
